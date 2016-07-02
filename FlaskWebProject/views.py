0349d98287052a83ef5cc423a2846c9346a2d39b
hernan <hquintana@devosinc.com>
2016-07-01 20:05:01 -0500

    Added endpoints

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -6,6 +6,34 @@ from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app
 
+from flask import Flask, jsonify,request
+from flask.ext.cors import CORS, cross_origin
+from pymongo import MongoClient
+from bson.json_util import dumps
+from bson import json_util
+import json
+
+@app.route('/listar/<coleccion>', methods=['GET'])
+def listar_ubicaciones(coleccion):
+	client = MongoClient('mongodb://ulima:ulima@ds011745.mlab.com:11745/db_rep')
+	db = client.db_rep
+
+	rpta = []
+
+	if coleccion == "mantenimientos":
+		rpta = list(db.mantenimientos.find())
+
+	if coleccion == "ubicaciones":
+		rpta = list(db.ubicaciones.find())
+
+	if coleccion == "equipos":
+		rpta = list(db.equipos.find())
+
+	if coleccion == "cpmms":
+		rpta = list(db.cpmms.find())
+
+	return  json.dumps(rpta, default=json_util.default)
+
 @app.route('/')
 @app.route('/home')
 def home():
